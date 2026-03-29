492df03660d701206b68ccfd416f6bfe64ab03e9
2k25csaiml2513533-gif <2k25csaiml2513533@gmail.com>
2026-03-29 23:11:30 +0530

    javascript codes

---
+++ modules/03_conversion.js
@@ -0,0 +1,24 @@
+// let score="33abc"
+// console.log(typeof score);
+// let valueinNumber=Number (score)
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber);
+// //"33" => 33
+// // "33abc" => NaN
+// // true=>1 ;false => 0
+
+// let somenum=33
+// let str=String(somenum)
+// console.log(typeof str);
+//*************** operations*********************** */
+let value=3
+let negval=-value
+// console.log(negval);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**2);
+console.log(2+"2");
